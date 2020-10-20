96a688eb86b1f2b4936ad6ea08b2aef1d23d3627
gjesionowski <gregory.a.jesionowski@gmail.com>
2020-10-20 13:28:08 -0700

    Adds contact layout and Contact classes

--- app/src/main/java/com/example/assignmentsix/MainActivity.java
+++ app/src/main/java/com/example/assignmentsix/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.assignmentsix;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.assignmentsix.Contact;
+import com.example.assignmentsix.ContactAdapter;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactAdapter adapter = new ContactAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 }
